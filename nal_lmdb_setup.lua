7d5b3af1a54a7a66398c638a0f9c9d48f6627ac7
Hiroaki Nakamura <hnakamur@gmail.com>
2023-03-27 15:31:02 +0900

    Add open_databases

--- nal_lmdb_setup.lua
+++ nal_lmdb_setup.lua
@@ -7,9 +7,9 @@ local function setup(shlib_name)
                          unsigned int max_readers, size_t map_size);
 
         typedef struct MDB_txn * nal_txn_ptr;
-        typedef unsigned int	 nal_dbi;
+        typedef unsigned int     nal_dbi;
         typedef struct nal_val {
-            size_t	    mv_size;
+            size_t      mv_size;
             const char *mv_data;
         } nal_val;
 
@@ -58,14 +58,8 @@ local function setup(shlib_name)
 
     local function readonly_txn_begin(parent)
         local txn = ffi.new(c_txn_ptr_type)
-    ::retry::
         local rc = S.nal_readonly_txn_begin(parent, txn)
         if rc ~= MDB_SUCCESS then
-            -- if rc == EAGAIN then
-            --     ngx.log(ngx.WARN, "nal_readonly_txn_begin retry after EAGAIN")
-            --     goto retry
-            -- end
-            ngx.log(ngx.ERR, string.format("nal_readonly_txn_begin rc=%d", rc))
             return nil, nal_strerror(rc)
         end
         return txn[0]
@@ -82,7 +76,6 @@ local function setup(shlib_name)
     local function txn_renew(txn)
         local rc = S.nal_txn_renew(txn)
         if rc ~= MDB_SUCCESS then
-            ngx.log(ngx.ERR, string.format("nal_txn_renew rc=%d", rc))
             return nal_strerror(rc)
         end
         return nil
@@ -123,7 +116,7 @@ local function setup(shlib_name)
             if rc == MDB_NOTFOUND then
                 return nil
             end
-            return nil, S.nal_strerror(rc)
+            return nil, nal_strerror(rc)
         end
         return ffi.string(nal_data[0].mv_data, nal_data[0].mv_size)
     end
@@ -148,7 +141,7 @@ local function setup(shlib_name)
         nal_key[0].mv_data = key
         local rc = S.nal_del(self, dbi, nal_key)
         if rc ~= 0 and rc ~= MDB_NOTFOUND then
-            return S.nal_strerror(rc)
+            return nal_strerror(rc)
         end
         return nil
     end
@@ -170,13 +163,11 @@ local function setup(shlib_name)
     end
 
     local ro_txns = {}
+    local dbis = {}
 
     local function get_ro_txn()
         local i = table.maxn(ro_txns)
         if i ~= 0 then
-            if i > 1 then
-                ngx.log(ngx.NOTICE, string.format("get_ro_txn: i=%d", i))
-            end
             local txn = ro_txns[i]
             ro_txns[i] = nil
             local err = txn_renew(txn)
@@ -190,7 +181,6 @@ local function setup(shlib_name)
     end
 
     local function put_ro_txn(txn)
-        S.nal_txn_reset(txn)
         table.insert(ro_txns, txn)
     end
 
@@ -201,32 +191,53 @@ local function setup(shlib_name)
         end
 
         local err = f(txn)
+        S.nal_txn_reset(txn)
         put_ro_txn(txn)
         return err
     end
 
-    local function get(key, db)
-        local val
-        local err = with_txn(nil, function(txn)
-            local dbi, err = txn:readonly_db_open(db)
+    local function open_databases(databases)
+        local txn, err = txn_begin(nil)
+        if err ~= nil then
+            return err
+        end
+
+        for i, db in ipairs(databases) do
+            local dbi
+            dbi, err = txn:db_open(db)
             if err ~= nil then
                 return err
             end
+            dbis[db] = dbi
+        end
 
-            val, err = txn:get(key, dbi)
-            if err ~= nil then
-                return err
+        err = txn_commit(txn)
+        if err ~= nil then
+            return err
+        end
+
+        return nil
+    end
+
+    local function get(key, db)
+        local val
+        local err = with_readonly_txn(function(txn)
+            local err2
+            val, err2 = txn:get(key, dbis[db])
+            if err2 ~= nil then
+                return err2
             end
 
             return nil
         end)
-	return val, err
+        return val, err
     end
 
     return {
         env_init = env_init,
         with_txn = with_txn,
         with_readonly_txn = with_readonly_txn,
+        open_databases = open_databases,
         get = get,
     }
 end
